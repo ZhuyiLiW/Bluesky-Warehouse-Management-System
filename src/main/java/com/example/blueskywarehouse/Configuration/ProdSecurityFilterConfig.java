3553a3a288f86b40370d9a359b7dca24bc2e0be7
Zhuyi Li W <lizhuyi29@gmail.com>
2025-08-10 14:47:37 +0200

    feat: update Security Strategie

--- src/main/java/com/example/blueskywarehouse/Configuration/ProdSecurityFilterConfig.java
+++ src/main/java/com/example/blueskywarehouse/Configuration/ProdSecurityFilterConfig.java
@@ -79,7 +79,7 @@ public class ProdSecurityFilterConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://192.168.178.94:3000","http://192.168.2.132:3000","http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://192.168.178.94:3000","http://192.168.2.132:3000""));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
